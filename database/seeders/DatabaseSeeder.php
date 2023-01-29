294f15454877dfbff260909128bdf16a1adf876b
William Niemiec <williamniemiec@hotmail.com>
2023-01-29 15:12:02 -0300

    call factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Appointment;
+use App\Models\Availability;
+use App\Models\Favorite;
+use App\Models\Photo;
+use App\Models\Review;
+use App\Models\Service;
+use App\Models\Testimonial;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,5 +23,14 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class
         ]);
+
+        Barber::factory(5)->create();
+        Appointment::factory(5)->create();
+        Availability::factory(5)->create();
+        Favorite::factory(3)->create();
+        Photo::factory(6)->create();
+        Review::factory(5)->create();
+        Service::factory(7)->create();
+        Testimonial::factory(5)->create();
     }
 }
