d98c50981cb3c997822287ef8b548f1e30042a73
William Niemiec <williamniemiec@hotmail.com>
2023-03-26 12:30:48 -0300

    real barber photos

--- database/factories/BarberPhotoFactory.php
+++ database/factories/BarberPhotoFactory.php
@@ -10,6 +10,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class BarberPhotoFactory extends Factory
 {
+    private static $barberPhotos = [
+        "https://nationaltoday.com/wp-content/uploads/2022/02/Barbers-Day.jpg",
+        "https://i2-prod.manchestereveningnews.co.uk/incoming/article21411590.ece/ALTERNATES/s615/0_gettyimages-1207048163-170667a.jpg",
+        "https://images.unsplash.com/photo-1599351431202-1e0f0137899a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFyYmVyJTIwc2hvcHxlbnwwfHwwfHw%3D&w=1000&q=80",
+        "https://blog.mensmarket.com.br/wp-content/uploads/2018/01/barbearia-barber-shop.jpg"
+    ];
+
     /**
      * Define the model's default state.
      *
@@ -18,10 +25,11 @@ class BarberPhotoFactory extends Factory
     public function definition()
     {
         $barber = Barber::all()->random();
+        $totalImages = count(BarberPhotoFactory::$barberPhotos);
 
         return [
             'id_barber' => $barber->id,
-            'url' => fake()->imageUrl()
+            'url' => BarberPhotoFactory::$barberPhotos[fake()->numberBetween(0, $totalImages-1)]
         ];
     }
 }
